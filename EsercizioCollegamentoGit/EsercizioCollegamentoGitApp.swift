4f73af130d2fe6caadf71c4cd03838fdc3b6598e
Bet <b.dellorusso@studenti.unisa.it>
2025-06-20 10:23:06 +0200

    Initial Commit

---
+++ EsercizioCollegamentoGit/EsercizioCollegamentoGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsercizioCollegamentoGitApp.swift
+//  EsercizioCollegamentoGit
+//
+//  Created by Studente on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct EsercizioCollegamentoGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
